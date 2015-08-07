e24a44327de434dbb88e753ab89d851b0401a5a7
tirapa-eidiko <tirapareddy.tondapu@eidiko.com>
2015-08-07 00:55:44 -0400

    Update HelloWorldServlet.java

--- src/main/java/net/viralpatel/maven/HelloWorldServlet.java
+++ src/main/java/net/viralpatel/maven/HelloWorldServlet.java
@@ -20,7 +20,7 @@ public class HelloWorldServlet extends HttpServlet {
 		resp.setContentType("text/html");
 		
 		PrintWriter out = resp.getWriter();
-		out.print("Hello World from TIrapa Reddy TondapuTTT");
+		out.print("Jenkins example");
 		out.flush();
 		out.close();
 	}
